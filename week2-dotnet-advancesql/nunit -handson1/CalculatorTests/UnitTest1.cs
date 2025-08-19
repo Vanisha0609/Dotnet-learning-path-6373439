a6476c7ada87747d7870fdfd34336ccb767c0850
vanisha.ramesh <119104009+Vanisha0609@users.noreply.github.com>
2025-08-19 11:05:18 +0530

    Add files via upload

---
+++ week2-dotnet-advancesql/nunit -handson1/CalculatorTests/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using CalcLibrary;
+
+namespace CalculatorTests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        private Calculator _sut;
+
+        [SetUp]
+        public void Setup() => _sut = new Calculator();
+
+        [Test]
+        public void Add_With1And2_Returns3()
+        {
+            var result = _sut.Add(1, 2);
+            Assert.That(result, Is.EqualTo(3));
+        }
+    }
+}
+
